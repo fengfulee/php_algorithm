cdb7241413ee0a45aa2d4f75771e0d305786abf4
= <=>
2014-03-09 12:54:59 +0800

    实现了ArrayAccess和Countable 接口

--- fengfulee.ArrayAccess_Countable.class.php
+++ fengfulee.ArrayAccess_Countable.class.php
@@ -1,3 +1,74 @@
 <?PHP
+#	这个类实现了 ArrayAccess 和 Countable 接口.
+#	这样就能够像数组一样对其进行赋值 和 使用 count() 函数了.
+#	并且这里使用了 Singleton单例模式....
+class Single implements ArrayAccess,Countable{
+
+	private $name;	#给它一个属性..
+	private static $_instance = null;#对象实例..
+	private $container = array();	#数组,用于存放数据...
+	#实现 单例模式.....
+	static function instance(){
+		if(null==self::$_instance){
+			#如果为空,则创建..否则直接返回该静态变量(实例化的对象...)
+			return	self::$_instance = new Single();
+		}
+		return self::$_instance;
+	}
+
+	#设置getter,setter 方法..
+	public function setName($name){
+		$this->name = $name;
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+
+	#ArrayAccess 需要实现4个方法..
+	#offsetExists(),标示一个元素是否存在.
+	#offsetGet(),获取一个元素..
+	#offsetSet(),设置一个元素..
+	#offsetUnset() 删除一个元素...
+	public function offsetSet($offset,$value){
+		if(is_null($offset))	$this->container[] = $value;
+		else			$this->container[$offset] = $value;
+	}
+
+	public function offsetGet($offset){
+		return isset($this->container[$offset])?$this->container[$offset]:null;
+	}
+	
+	public function offsetExists($offset){
+		return isset($this->container[$offset]);
+	}
+
+	public function offsetUnset($offset){
+		unset($this->container[$offset]);
+	}
+
+	#Countable接口需要实现count方法...
+	public function count(){
+		return sizeof($this->container);	#sizeof是 count的别名方法...
+	}
+}//end of clazz...
+
+
+	#t################test################
+	$s = Single::instance();
+	$s-> setName('fengfulee');
+	echo $s->getName(),"\n";
+	$s['name'] = 'fengfulee';
+	$s['sex'] = 'man';
+	$s['age'] = 23;
+	$s['job'] = 'student';
+	echo $s['name'],',',$s['sex'],',',$s['age'],',',$s['job'],"\n";
+	
+	echo count($s),"\n";
+
+
+
+
+
 
 ?>
--- shellSort.php
+++ shellSort.php
@@ -1,3 +1,36 @@
 <?PHP
+#	希尔排序算法... 先取一个小于n的整数d1作为第一个增量，把文件的全部记录分成d1个组。所有距离为d1
 
+#	的倍数的记录放在同一个组中。先在各组内进行直接插入排序；然后，取第二个增量d2<d1重复
+
+#	上述的分组和排序，直至所取的增量dt=1(dt<dt-l<…<d2<d1)，即所有记录放在同一组中进行
+
+#	`直接插入排序为止。
+	function shellSort($array){
+		#如果不是数组,则返回...
+		if(!is_array($array))	return false;
+		
+		$len = sizeof($array);	#sizeof 是count的别名函数 ...alias..
+		$d = $len;
+		while($d>1){		#循环终结在$d=1;
+			$d = intval($d/2);
+			$temp = NULL;
+			$j=0;
+			for($i=$d; $i<$len;$i+=$d){
+				if($array[$i]<$array[$i-$d]){
+					$temp = $array[$i];
+					$j = $i-$d;
+					while($j>=0&&$temp<$array[$j]){
+						$array[$j+$d] = $array[$j];
+						$j= $j-$d;
+					}
+					$array[$j+$d] = $temp;
+				}
+			}//end of for 
+		}
+		return $array;
+	}
+
+	$array = array(7,2,4,1,6,3,8,0,5);
+	print_r(shellSort($array));
 ?>
